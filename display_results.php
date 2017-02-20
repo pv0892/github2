3e76737029f0957b719353745615da1737885147
Pranav Bawa <pb389@afsaccess1.njit.edu>
2017-02-20 00:38:24 -0500

    date added

--- display_results.php
+++ display_results.php
@@ -69,6 +69,9 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
-    </main>
+   <p>this calculation was  done on<?php echo date('m/d/Y');?>.
+   </p>
+   </main>
 </body>
 </html>
+
